cf18fa1265e76b636884bbca429eb4390419c9c5
kitralzhong <784248377@qq.com>
2018-04-02 08:25:32 +0800

    fix

--- main.go
+++ main.go
@@ -2,38 +2,74 @@ package main
 
 import (
 	"fmt"
+	"time"
+	"math/rand"
 )
 
-func main()  {
-	// 无缓冲信道存消息和取消息都是阻塞的
-	// 取消息会阻塞当前routine
-	// 写如消息也会阻塞当前routine
-	// 开启一个not buffered channel的时候 准备读取的时候 必须有数据进来 否则就会阻塞住一直等待数据写入
-	// 对一个not buffered channel 进行写入操作的时候 如果数据没有被读出 则channel是满的 该routine一直会被挂起 知道数据被取出
-	// 对于一个 not buffered channel 来说 读取大部分下是成对的
-
-	// channel的作用可以用来通知main routine当前的routine已经完毕了 你可以退出了 例子如下
-	/*
-	ch := make(chan struct{})
-	go func() {
-		fmt.Println("handle 1 second...")
-		time.Sleep(time.Second)
-		ch <- struct{}{}
-	}()
-	<-ch
-	fmt.Println("ok")
-	// handle 1 second...
-	// ok
-	*/
-
-	// 引起死锁 一个routine在等待数据 一个迟迟不肯写入数据 或者相反比如
-	// 一般思维看下来 先写入到了ch 然后就把ch读出来非常完美不是吗？错
-	// ch <- 1 写入的过程 如果ch的数据没有被取出去 这里就会被阻塞住了
-	// 然后后面的的 <-ch 就一直在等着当前routine的释放 形成死锁deadlock
-	/*
+/*
+func xrange() chan int{
 	ch := make(chan int)
-	ch <- 1
-	fmt.Println(<-ch)
-	*/
+	go func() {
+		for i:=0;;i++{
+			ch <- i
+		}
+	}()
+	return ch
+}
 
+func get_msg(user string) chan string{
+	ch := make(chan string)
+	go func() {
+		ch <- fmt.Sprintf("%s, Hello World\n", user)
+	}()
+	return ch
+}
+*/
+/*
+func do_stuff(i int)int  {
+	time.Sleep(time.Duration(rand.Intn(10)) * time.Millisecond)
+	return 100 - i
+}
+func branch(i int) chan int{
+	ch := make(chan int)
+	go func() {
+		ch <- do_stuff(i)
+	}()
+	return ch
+}
+func fanIn(chs... chan int) chan int{
+	ch := make(chan int)
+	for _,c := range chs{
+		go func(c chan int) {
+			//
+			ch <- <- c
+		}(c)
+	}
+	return ch
+}
+*/
+
+func main()  {
+	// 应用-生成器 模拟python的xrange 只有登到数据需要的时候才生成数据adf
+	/*
+	generator := xrange()
+	for i:=0; i < 1000; i++{
+		fmt.Println(<-generator)
+	}
+	*/
+	// 应用-服务话
+	/*
+	jack := get_msg("jack")
+	lartik := get_msg("lartik")
+	fmt.Println(<-jack)
+	fmt.Println(<-lartik)
+	*/
+	// 应用 多路复合 可以将多个channel的数据合并到一个数据
+	/*
+	result := fanIn(branch(1), branch(2), branch(3))
+	for i:=0; i<3; i++ {
+		fmt.Println(<-result)
+	}
+	*/
+	
 }
